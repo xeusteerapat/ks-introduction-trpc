817d48a43b26154806da5b831ff28075a6e63f24
xeusteerapat <xeus085@gmail.com>
2023-03-17 16:47:04 +0700

    :lipstick: [Update] setup mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,33 @@
 import type { AppProps } from 'next/app';
 import { trpc } from '../libs/trpc';
-import 'marx-css';
+import Head from 'next/head';
+import { MantineProvider } from '@mantine/core';
 
-function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const App = (props: AppProps) => {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width'
+        />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'light',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
+};
 
 export default trpc.withTRPC(App);
